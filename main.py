64bc87c8884582767b66fa884d48019bca517423
vishnuvardhanj <vishnuvardhanj@google.com>
2021-11-04 21:03:18 -0700

    My first commit
    My first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import Any, Union
+
+from tensorflow import Tensor
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    import tensorflow as tf
+    a = tf.constant(1.)
+    b = tf.constant(2.)
+    print(tf.math.reduce_sum(a+b))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
